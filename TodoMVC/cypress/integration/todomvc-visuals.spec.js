6b02f9921917f59405eb800ba93214b73ee575e4
binmurv <binmurv@gmail.com>
2021-05-10 21:53:07 +0800

    Visual validation using Applitools Eyes

---
+++ TodoMVC/cypress/integration/todomvc-visuals.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+import { TodoPage } from "../page-objects/todo-page"
+
+describe('visual validation', () => {
+    const todoPage = new TodoPage()
+    before(() => todoPage.navigate())
+
+    beforeEach(() => cy.eyesOpen({
+        appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!',
+        browser: [
+            {name: 'chrome', width: 1026, height: 768},
+            {name: 'chrome', width: 800, height: 600},
+            {name: 'chrome', width: 1026, height: 768},
+            {deviceName: 'iPhone X'},
+        ]
+    }))
+    afterEach(() => cy.eyesClose())
+
+    it('should look good', () => {
+        cy.eyesCheckWindow('empty todo list')
+
+        todoPage.addTodo('Learn Cypress')
+        todoPage.addTodo('Learn Robot FW')
+
+        cy.eyesCheckWindow('two todos')
+
+        todoPage.toggleTodo(0)
+
+        cy.eyesCheckWindow('mark as completed')
+    })
+})
